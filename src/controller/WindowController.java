bd08bc532e336fcffa6530157c91e8611d93602b
Manuel David Castano Saldarriaga <1005966201@academico.icesi.edu.co>
2020-02-14 13:00:17 -0500

    thread

--- src/controller/WindowController.java
+++ src/controller/WindowController.java
@@ -8,6 +8,7 @@ import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.GridPane;
 import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
@@ -19,78 +20,53 @@ public class WindowController implements Initializable{
 	@FXML
 	private TextField tfTop;
 	@FXML
-	private GridPane gp;
-	@FXML
 	private Button b1;
 	@FXML
 	private Button b2;
 	@FXML
 	private Button b3;
-	private ArrayList<Label> al;
+	@FXML
+	private AnchorPane ap;
 
 	@Override
 	public void initialize(URL arg0, ResourceBundle arg1) {
 		b1.setOnAction(e -> {
-			generateMatrix();
-			color(1);
+			generateMatrix(1);
 		});
 		b2.setOnAction(e -> {
-			generateMatrix();
-			color(2);
+			generateMatrix(2);
 		});
 		b3.setOnAction(e -> {
-			generateMatrix();
-			color(3);
+			generateMatrix(3);
 		});
-		al = new ArrayList<Label>();
 	}
 	
-	public void generateMatrix() {
-		gp.getChildren().clear();
+	public void generateMatrix(int method) {
+		ap.getChildren().clear();
 		int top = Integer.parseInt(tfTop.getText());
-		number = new Numbers(top);
-		number.matrixSize();
+		number = new Numbers(top, method);
 		int aux = 1;
 		for (int i = 0; i < number.getMatrix().length; i++) {
 			for (int j = 0; j < number.getMatrix()[0].length && aux <= top; j++) {
 				Label l = new Label("" + aux);
+				l.setLayoutX(i*30);
+				l.setLayoutY(j*30);
 				l.setFont(new Font(15));
-				l.setMinHeight(40);
-				l.setMinWidth(40);
-				al.add(l);
-				gp.add(l, j, i);
+				if(number.getBooleans()[i][j]) {
+					l.setTextFill(Color.GREEN);
+				} else {
+					l.setTextFill(Color.RED);
+				}
+				ap.getChildren().add(l);
+				
+				try {
+					Thread.currentThread().sleep(1000);
+					System.out.println(Thread.currentThread().getName());
+					System.out.println(ap.getChildren().toString());
+				} catch (Exception e) {
+					
+				}
 				aux++;
-			}
-		}
-	}
-	
-	public void color(int method) {
-		for (int i = 0; i < al.size(); i++) {
-			int theNumber = Integer.parseInt(al.get(i).getText());
-			if(method == 1) {
-				if(number.isPrimeNumber1(theNumber)) {
-					al.get(i).setTextFill(Color.GREEN);
-				} else {
-					al.get(i).setTextFill(Color.RED);
-				}
-			} else if(method == 2) {
-				if(number.isPrimeNumber2(theNumber)) {
-					al.get(i).setTextFill(Color.GREEN);
-				} else {
-					al.get(i).setTextFill(Color.RED);
-				}
-			} else {
-				if(number.isPrimeNumber3(theNumber)) {
-					al.get(i).setTextFill(Color.GREEN);
-				} else {
-					al.get(i).setTextFill(Color.RED);
-				}
-			}
-			try {
-				Thread.sleep(500);
-			} catch (InterruptedException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
 			}
 		}
 	}
--- src/model/Numbers.java
+++ src/model/Numbers.java
@@ -4,9 +4,15 @@ public class Numbers {
 	
 	private int top;
 	private int [][] matrix;
+	private boolean [][] booleans;
+	private int method;
 	
-	public Numbers(int top) {
+	public Numbers(int top, int method) {
 		this.top = top;
+		matrixSize();
+		booleans = new boolean[matrix.length][matrix[0].length];
+		fillBooleans();
+		this.method = method;
 	}
 
 	public int getTop() {
@@ -31,6 +37,13 @@ public class Numbers {
 			matrix = new int[(int) root][(int) root];
 		} else {
 			matrix = new int[(int) root+1][(int) root+1];
+		}
+		int aux = 1;
+		for(int i = 0; i < matrix.length; i++) {
+			for(int j = 0; j < matrix[0].length; j++) {
+				matrix[i][j] = aux;
+				aux++;
+			}
 		}
 	}
 	
@@ -67,6 +80,22 @@ public class Numbers {
 	    return prime;
 	}
 	
+	public boolean[][] getBooleans() {
+		return booleans;
+	}
+
+	public void setBooleans(boolean[][] booleans) {
+		this.booleans = booleans;
+	}
+
+	public int getMethod() {
+		return method;
+	}
+
+	public void setMethod(int method) {
+		this.method = method;
+	}
+
 	public boolean isPrimeNumber3(int n){
 		int a = 0;
 		for (int i = 1; i <= n; i++) {
@@ -78,5 +107,19 @@ public class Numbers {
 			return false;
 		else 
 			return true;
+	}
+	
+	public void fillBooleans() {
+		for(int i = 0; i < booleans.length; i++) {
+			for(int j = 0; j < booleans[0].length; j++) {
+				if(method == 1) {
+					booleans[i][j] = isPrimeNumber1(matrix[i][j]);
+				} else if(method == 2) {
+					booleans[i][j] = isPrimeNumber2(matrix[i][j]);
+				} else {
+					booleans[i][j] = isPrimeNumber3(matrix[i][j]);
+				}
+			}
+		}
 	}
 }
